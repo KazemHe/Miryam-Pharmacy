fe0f08593bf7b778a994a12342472217d40b5ff1
KazemHe <kazemhe1@gmail.com>
2023-09-19 14:45:44 +0300

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import main from './style/main.scss'
+
 import Header from './cmp/Header';
 import Footer from './cmp/Footer';
 import HomePage from './pages/HomePage';
-
-
-
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
 <Header />
       </header>
-< HomePage />
-
+      <Routes>
+      <Route path="/" element={<HomePage />} />
+      </Routes>
 
       <Footer />
     </div>
+    </Router>
   );
 }
 
